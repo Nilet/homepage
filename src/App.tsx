47111ea1c2e4fda6a1548af97f96487937daf0e3
Nilet <nilet@tutanota.com>
2023-04-11 19:47:09 -0300

    Added yandex image search button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import yandexLogo from './assets/yandex.svg'
 import bingLogo from './assets/bing.svg'
 import googleLogo from './assets/google.svg'
-import cameraSvg from './assets/camera.svg'
 import { useState } from 'react'
 import SearchEngineSelector from './components/SearchEngineSelector'
 import './App.css'
@@ -62,7 +61,12 @@ function App() {
                 />
             </div>
             <input type="search" placeholder="Search..." autoFocus id="SearchField" onKeyDown={submitSearch} />
-            <a id="ReverseImageSearch"> <img src={cameraSvg}></img></a>
+            <a href='https://yandex.com/images/' id="ReverseImageSearch">
+                <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M21 13C21 10.3333 20.5 8 20 7.66667C19.6796 7.45303 18.1268 7.2394 16 7.11352C14.8083 7.04298 17 5 12 5C7 5 9.19168 7.04298 8 7.11352C5.87316 7.2394 4.32045 7.45303 4 7.66667C3.5 8 3 10.3333 3 13C3 15.6667 3.5 18 4 18.3333C4.5 18.6667 8 19 12 19C16 19 19.5 18.6667 20 18.3333C20.5 18 21 15.6667 21 13Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M12 16C13.6569 16 15 14.6569 15 13C15 11.3431 13.6569 10 12 10C10.3431 10 9 11.3431 9 13C9 14.6569 10.3431 16 12 16Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+            </a>
         </div>
     );
 }
