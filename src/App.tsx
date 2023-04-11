9a3a7a1717ee4083810a439441605b8f1281ed60
Nilet <nilet@tutanota.com>
2023-04-11 15:16:00 -0300

    Finished search engines

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,68 @@
 import yandexLogo from './assets/yandex.svg'
 import bingLogo from './assets/bing.svg'
 import googleLogo from './assets/google.svg'
+import { useState } from 'react'
 import SearchEngineSelector from './components/SearchEngineSelector'
 import './App.css'
 
+const yandexUrl = "https://yandex.com/search/?text=";
+const bingUrl = "https://www.bing.com/search?q=";
+const googleUrl = "https://www.google.com/search?q=";
+
 function App() {
+    const [selectedEngine, setSelectedEngine] = useState("Google");
+
+    const handleEngineSelect = (engineName: string) => {
+        setSelectedEngine(engineName);
+        console.log(engineName);
+    };
+
+    const submitSearch = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === "Enter") {
+            const inputValue = event.currentTarget.value;
+            let url = "";
+            switch (selectedEngine) {
+                case "Yandex":
+                    url = yandexUrl + inputValue
+                    break;
+                case "Bing":
+                    url = bingUrl + inputValue
+                    break;
+                case "Google":
+                    url = googleUrl + inputValue
+                        break;
+                default:
+                    break;
+            }
+            window.location.href = url;
+        }
+    }
 
     return (
         <div className="App">
-            <div className='SearchEngineWrapper'>
-                <SearchEngineSelector logo={googleLogo} name='Google'></SearchEngineSelector>
-                <SearchEngineSelector logo={yandexLogo} name='Yandex'></SearchEngineSelector>
-                <SearchEngineSelector logo={bingLogo} name='Bing'></SearchEngineSelector>
+            <div className="SearchEngineWrapper">
+                <SearchEngineSelector
+                    logo={googleLogo}
+                    name="Google"
+                    selected={selectedEngine === "Google"}
+                    onSelect={handleEngineSelect}
+                />
+                <SearchEngineSelector
+                    logo={yandexLogo}
+                    name="Yandex"
+                    selected={selectedEngine === "Yandex"}
+                    onSelect={handleEngineSelect}
+                />
+                <SearchEngineSelector
+                    logo={bingLogo}
+                    name="Bing"
+                    selected={selectedEngine === "Bing"}
+                    onSelect={handleEngineSelect}
+                />
             </div>
+            <input type="search" placeholder="Search..." autoFocus id="SearchField" onKeyDown={submitSearch} />
         </div>
-    )
+    );
 }
 
-export default App
+export default App;
--- src/components/SearchEngineSelector.tsx
+++ src/components/SearchEngineSelector.tsx
@@ -1,9 +1,21 @@
-function SearchEngineSelector(props: { logo: string, name: string}) {
+function SearchEngineSelector(props: {
+    logo: string;
+    name: string;
+    selected: boolean;
+    onSelect: (name: string) => void;
+}) {
     return (
-    <button className="SearchEngineButton">
-        <img src={props.logo} alt={props.name + " logo"} className="SearchEngineLogo"/>
-        <span className="SearchEngineName">{props.name}</span>
-    </button>
+        <button
+            className={`SearchEngineButton ${props.selected ? "SelectedSearchEngine" : ""}`}
+            onClick={() => props.onSelect(props.name)}
+        >
+            <img
+                src={props.logo}
+                alt={`${props.name} logo`}
+                className="SearchEngineLogo"
+            />
+            <span className="SearchEngineName">{props.name}</span>
+        </button>
     );
 }
 
