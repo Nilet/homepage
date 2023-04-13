da02c0db414566d0ebe5f5ce1b20d6a6df5a2767
Nilet <nilet@tutanota.com>
2023-04-13 11:25:15 -0300

    Added some stuff, still deciding colorscheme

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import SearchEngineSelector from './components/SearchEngineSelector'
 import Links from './components/LinksWrapper'
+import lain from './assets/lain.gif'
 
 import yandexLogo from './assets/yandex.svg'
 import googleLogo from './assets/google.svg'
@@ -11,6 +12,7 @@ const googleUrl = "https://www.google.com/search?q=";
 
 function App() {
     const [selectedEngine, setSelectedEngine] = useState("Yandex");
+
 
     const submitSearch = (event: React.KeyboardEvent<HTMLInputElement>) => {
         if (event.key === "Enter") {
@@ -45,15 +47,14 @@ function App() {
                     selected={selectedEngine === "Google"}
                     onSelect={setSelectedEngine}
                 />
+                <div className='searchBarAndReverse'>
+                    <input type="search" placeholder="Search..." autoFocus id="SearchField" onKeyDown={submitSearch} />
+                    <a href='https://yandex.com/images/' id="ReverseImageSearch">
+                    </a>
+                </div>
             </div>
-            <input type="search" placeholder="Search..." autoFocus id="SearchField" onKeyDown={submitSearch} />
-            <a href='https://yandex.com/images/' id="ReverseImageSearch">
-                <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M21 13C21 10.3333 20.5 8 20 7.66667C19.6796 7.45303 18.1268 7.2394 16 7.11352C14.8083 7.04298 17 5 12 5C7 5 9.19168 7.04298 8 7.11352C5.87316 7.2394 4.32045 7.45303 4 7.66667C3.5 8 3 10.3333 3 13C3 15.6667 3.5 18 4 18.3333C4.5 18.6667 8 19 12 19C16 19 19.5 18.6667 20 18.3333C20.5 18 21 15.6667 21 13Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                    <path d="M12 16C13.6569 16 15 14.6569 15 13C15 11.3431 13.6569 10 12 10C10.3431 10 9 11.3431 9 13C9 14.6569 10.3431 16 12 16Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                </svg>
-            </a>
-           <Links/> 
+            <Links />
+            <img className='lain' src={lain} />
         </div>
     );
 }
